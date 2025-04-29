14e7235f37f17558837bde9b31e275ff90d2589f
a-barrel <darrelmd2000@gmail.com>
2025-04-28 22:30:12 -0700

    Finished 'calculate_avg_fridge_moisture' and 'calculate_avg_water_consuption_per_cycle_dishwasher'

--- server.py
+++ server.py
@@ -6,53 +6,66 @@ import psycopg2 # DARREL: For connecting to Neon and SQL
 
 # GOALS:
 # 1. Impliment a function for each of these
-#   a. What is the average moisture inside my kitchen fridge in the past three hours?
-#   b. What is the average water consumption per cycle in my smart dishwasher?
 #   c. Which device consumed more electricity among my three IoT devices (two refrigerators and a dishwasher)?
 # 2. Impliment a menu that returns the results of these functions
 
 def calculate_avg_fridge_moisture(connection):
+    # DARREL: This function calls a query onto the passed connection peram, query calculates the average and prints it
+    # DARREL: The units are relative, with values ranging between 0 (0%) and 40 (100%)
     cursor = connection.cursor()
     cursor.execute("""
-                   SELECT payload->>'Moisture Meter - moist-sensor_refrigerator_01' AS moisture_percent 
-                   FROM neon_table_virtual WHERE payload->>'board_name' = 'arduino_refrigerator_01' 
+                   SELECT AVG((payload->>'Moisture Meter - moist-sensor_refrigerator_01')::numeric) AS moisture
+                   FROM neon_table_virtual 
+                   WHERE payload->>'board_name' = 'arduino_refrigerator_01' 
                    AND time > NOW() - INTERVAL '3 hours'; 
                    """)
-    print(cursor.fetchone())
+    out = cursor.fetchone()[0]
     cursor.close()
-
-    print("FUNCTION CALLED: calculate_avg_fridge_moisture")
+    return(out)
 
 def calculate_avg_water_consuption_per_cycle_dishwasher(connection):
-    print("FUNCTION CALLED: calculate_avg_water_consuption_per_cycle_dishwasher")
+    # DARREL: This function calls a query onto the passed connection peram, query calculates the average and prints it
+    # DARREL: The units are leters per min
+    cursor = connection.cursor()
+    cursor.execute("""
+                   SELECT AVG((payload->>'YF-S201 - water-flow-sensor_dishwasher_01')::numeric) AS moisture_percent 
+                   FROM neon_table_virtual 
+                   WHERE payload->>'board_name' = 'arduino_dishwasher_01' 
+                   AND time > NOW() - INTERVAL '3 hours'; 
+                   """)
+    out = cursor.fetchone()[0]
+    cursor.close()
+    return(out)
 
 def calculate_most_electicity_consumed(connection):
-    print("FUNCTION CALLED: calculate_most_electicity_consumed")
+    # DARREL: This function calls a query onto the passed connection peram, query calculates the average and prints it
+    # DARREL: The units are leters per min
+    print("--FUNCTION CALLED: calculate_most_electicity_consumed--")
+    return(0)
 
-def inital_test():
+def inital_test(connection):
     # DARREL: This function will call each function, output their results, then go to the start_server function.
     # DARREL: Using this primarly to just test the functions without connecting a client.
-
-    try:
-        connecturl = "postgresql://neondb_owner:npg_8cHZMibC2QVt@ep-curly-cloud-a6wcg53n-pooler.us-west-2.aws.neon.tech/neondb?sslmode=require"
-        connection = psycopg2.connect(connecturl)
-
-        print("CALLING FUNCTION: calculate_avg_fridge_moisture")
-        calculate_avg_fridge_moisture(connection)
-
-        print("CALLING FUNCTION: calculate_avg_water_consuption_per_cycle_dishwasher")
-        calculate_avg_water_consuption_per_cycle_dishwasher(connection)
-
-        print("CALLING FUNCTION: calculate_most_electicity_consumed")
-        calculate_most_electicity_consumed(connection)
-
-        connection.close()
-    
-    except Exception as error:
-        print(f"Error connecting to SQL: {error}")
+    print("")
+    print("RUNING INITAL TESTS:")
+    print("     --CALLING FUNCTION: calculate_avg_fridge_moisture--")
+    print(calculate_avg_fridge_moisture(connection))
+    print("     --CALLING FUNCTION: calculate_avg_water_consuption_per_cycle_dishwasher--")
+    print(calculate_avg_water_consuption_per_cycle_dishwasher(connection))
+    print("     --CALLING FUNCTION: calculate_most_electicity_consumed--")
+    print(calculate_most_electicity_consumed(connection))
+    print("")
 
 def start_server():
     try:
+        # DARREL: Setup connection
+        connecturl = "postgresql://neondb_owner:npg_8cHZMibC2QVt@ep-curly-cloud-a6wcg53n-pooler.us-west-2.aws.neon.tech/neondb?sslmode=require"
+        connection = psycopg2.connect(connecturl)
+
+        # DARREL: Run every function just to test database connection and queries!
+        inital_test(connection)
+
+        # DARREL: This the old server code block!
         myTCPSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         # Allow user to enter a port instead of hardcoding
         serverPort = int(input("Enter the port number to run the server on: "))
@@ -71,11 +84,14 @@ def start_server():
             # Convert the received message to uppercase and send it back
             response = myData.upper()
             incomingSocket.send(bytearray(response, encoding='utf-8'))
+
+        # DARREL: Close connection
+        connection.close()
+
     except Exception as e:
         print(f"An error occurred: {e}")
         incomingSocket.close() # Close client connection
         myTCPSocket.close() # Close server socket
 
 if __name__ == "__main__":
-    inital_test()
     start_server()
